5f200ece9160d2dd7b34334317814ea231f5de1f
rktldhkd <rktldhkd@naver.com>
2018-09-14 16:59:06 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    # render() : 이 함수는 호출하여 받은(return) blog/post_list.html템플릿을 보여줍니다.
+    return render(request, 'blog/post_list.html', {})
